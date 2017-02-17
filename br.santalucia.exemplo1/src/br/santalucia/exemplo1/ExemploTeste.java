efbdacc5687d610bae7164bb855d770c2ed2b743
maromo71 <professormoraes@gmail.com>
2017-02-16 22:29:52 -0200

    Aula de 16 de fev

---
+++ br.santalucia.exemplo1/src/br/santalucia/exemplo1/ExemploTeste.java
@@ -0,0 +1,19 @@
+/*
+ * Aulas professor Maromo.
+ */
+package br.santalucia.exemplo1;
+
+/**
+ *
+ * @author maromo
+ */
+public class ExemploTeste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ br.santalucia.exemplo1/src/br/santalucia/exemplo1/Produto.java
@@ -0,0 +1,41 @@
+package br.santalucia.exemplo1;
+
+public class Produto {
+    private int id;
+    private String descricao;
+    private double valor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    /***
+     * @since 16 fev Atualizado o método
+     * listarProduto(). Agora ele apresenta
+     * todos os dados do objeto.
+     */
+    public void listarProduto(){
+    
+    }
+    
+}
